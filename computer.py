99e9ec8e33ed90b0a013cdf71be1d7ebbb4c22d7
Mkiterian <hmukite@gmail.com>
2017-06-13 19:09:40 +0300

    Implement Laptop class in computer module

--- computer.py
+++ computer.py
@@ -24,3 +24,28 @@ class Computer():
     def get_warranty(self, elapsed_months=0):
         self.__warranty -= elapsed_months
         return self.__warranty
+
+class Laptop(Computer):
+    def __init__(self, brand, memory_size, storage_size, os_type, battery_life):
+        super().__init__(brand, memory_size, storage_size, os_type)
+        self.battery_life = battery_life
+
+    def show_battery_life(self):
+        return 'The battery lasts up to {} Hours'.format(self.battery_life)
+
+    def can_run_windows_ten(self):
+        if self.memory_size > 1 and self.storage_size > 100:
+            if self.os_type != 'Windows 10':
+                return True
+            else:
+                return False
+        return False
+
+    def power_up(self):
+        return 'The Laptop is booting'
+
+    def shutdown(self):
+        return 'The Laptop is shutting down'
+
+    def install_drivers(self):
+        return 'Drivers are being installed'
